bdc2ea761211573276f557377d5c3e304bd5b39f
o_w <womarow@gmail.com>
2024-01-14 02:54:23 +0300

    dragg capability

--- js-chess/app.js
+++ js-chess/app.js
@@ -21,6 +21,7 @@ function createBoard(){
         const square = document.createElement('div')
         square.classList.add('square')
         square.innerHTML = startPiece
+        square.firstChild?.setAttribute('draggable', true)
         square.setAttribute('square-id', i)
         // square.classList.add('beige')
         const row = Math.floor((63 - i) / 8) + 1
@@ -39,3 +40,12 @@ function createBoard(){
     })
 }
 createBoard()
+const allSquares = document.querySelectorAll("#gameboard .square")
+
+allSquares.forEach(square => {
+    square.addEventListener('dragstart', dragStart)
+})
+
+function  dragStart(e) {
+    console.log(e)
+}
